c3660bafd6968181c7ff335e7efe3ea0e1d15c60
gonexwind <fikky.ardi15@gmail.com>
2021-10-31 17:13:25 +0700

    add bloc and testing

---
+++ test/presentation/bloc/search_bloc_test.dart
@@ -0,0 +1,81 @@
+import 'package:bloc_test/bloc_test.dart';
+import 'package:dartz/dartz.dart';
+import 'package:ditonton/common/failure.dart';
+import 'package:ditonton/domain/entities/movie.dart';
+import 'package:ditonton/domain/usecases/search_movies.dart';
+import 'package:ditonton/presentation/bloc/search_bloc.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+
+import '../provider/movie_search_notifier_test.mocks.dart';
+
+@GenerateMocks([SearchMovies])
+void main() {
+  late SearchBloc searchBloc;
+  late MockSearchMovies mockSearchMovies;
+
+  setUp(() {
+    mockSearchMovies = MockSearchMovies();
+    searchBloc = SearchBloc(mockSearchMovies);
+  });
+
+  test('initial state should be empty', () {
+    expect(searchBloc.state, SearchEmpty());
+  });
+
+  final tMovieModel = Movie(
+    adult: false,
+    backdropPath: '/muth4OYamXf41G2evdrLEg8d3om.jpg',
+    genreIds: [14, 28],
+    id: 557,
+    originalTitle: 'Spider-Man',
+    overview:
+        'After being bitten by a genetically altered spider, nerdy high school student Peter Parker is endowed with amazing powers to become the Amazing superhero known as Spider-Man.',
+    popularity: 60.441,
+    posterPath: '/rweIrveL43TaxUN0akQEaAXL6x0.jpg',
+    releaseDate: '2002-05-01',
+    title: 'Spider-Man',
+    video: false,
+    voteAverage: 7.2,
+    voteCount: 13507,
+  );
+  final tMovieList = <Movie>[tMovieModel];
+  final tQuery = 'spiderman';
+
+  blocTest<SearchBloc, SearchState>(
+    'Should emit [Loading, HasData] when data is gotten successfully',
+    build: () {
+      when(mockSearchMovies.execute(tQuery))
+          .thenAnswer((_) async => Right(tMovieList));
+      return searchBloc;
+    },
+    act: (bloc) => bloc.add(OnQueryChanged(tQuery)),
+    wait: Duration(milliseconds: 100),
+    expect: () => [
+      SearchLoading(),
+      SearchHasData(tMovieList),
+    ],
+    verify: (bloc) {
+      verify(mockSearchMovies.execute(tQuery));
+    },
+  );
+
+  blocTest<SearchBloc, SearchState>(
+    'Should emit [Loading, Error] when data is gotten unsuccessfully',
+    build: () {
+      when(mockSearchMovies.execute(tQuery))
+          .thenAnswer((_) async => Left(ServerFailure('Server Failure')));
+      return searchBloc;
+    },
+    act: (bloc) => bloc.add(OnQueryChanged(tQuery)),
+    wait: Duration(milliseconds: 100),
+    expect: () => [
+      SearchLoading(),
+      SearchError('Server Failure'),
+    ],
+    verify: (bloc) {
+      verify(mockSearchMovies.execute(tQuery));
+    },
+  );
+}
