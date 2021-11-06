b6cbd56463c6bf86b939cb1bf42f4659f3eb1502
gonexwind <fikky.ardi15@gmail.com>
2021-11-06 19:39:52 +0700

    add firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:core/core.dart';
 import 'package:ditonton/injection.dart' as di;
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -10,7 +11,7 @@ import 'package:tv_series/tv_series.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await HttpSSLPinning.init();
-
+  await Firebase.initializeApp();
   di.init();
   runApp(MyApp());
 }
--- movies/lib/presentation/pages/home_movie_page.dart
+++ movies/lib/presentation/pages/home_movie_page.dart
@@ -1,5 +1,6 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:core/core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
@@ -64,6 +65,7 @@ class _HomeMoviePageState extends State<HomeMoviePage>
         actions: [
           IconButton(
             onPressed: () {
+              // FirebaseCrashlytics.instance.crash();
               Navigator.pushNamed(context, SearchPage.ROUTE_NAME);
             },
             icon: Icon(Icons.search),
